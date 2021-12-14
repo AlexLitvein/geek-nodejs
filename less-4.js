43f59bb675ef2bceb688ebcd86e912740029e2c8
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-12-14 13:22:05 +0700

    less-4

--- less-4.js
+++ less-4.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
+const readline = require("readline");
 
 let currentDirectory = process.cwd();
 
@@ -10,44 +11,72 @@ const isFile = fileName => {
     return fs.lstatSync(fileName).isFile();
 }
 
-// const list = fs.readdirSync(currentDirectory).filter(isFile);
-// const list = fs.readdirSync(currentDirectory);
-
-// inquirer
-//     .prompt([
-//         {
-//             name: "fileName",
-//             type: "list",
-//             message: "Choose file:",
-//             choices: list,
-//         },
-//     ])
-//     .then((answer) => {
-//         const filePath = path.join(currentDirectory, answer.fileName);
-
-//         fs.readFile(filePath, 'utf8', (err, data) => {
-//             console.log(data);
-//         });
-//     });
-
 function findInStream(fileName, pattern) {
+    let count = 0;
     const readStream = fs.createReadStream(fileName,
         {
             encoding: 'utf-8',
             // highWaterMark: 32,
         });
 
-    readStream.on('data', (chunk) => {
-        // const arr = (tailChunk + chunk).split('\n');
-        // tailChunk = arr.pop();
+    const rl = readline.createInterface({ input: readStream, terminal: true });
 
-        // arr.forEach(el => {
-        //     elHandler(el, finds);
-        // });
-        if (chunk.includes(pattern)) {
-            console.log('finds!');
+    rl.on('line', (line) => {
+        const res = line.match(pattern);
+        if (res) {
+            console.log(`Совпадение найдено в ${count}-ой строке по индексу: ${res.index}`);
+            count++;
         }
     });
+
+    readStream.on('error', (err) => {
+        console.log(err.message);
+    });
+
+    readStream.on('end', () => {
+        console.log(`Найдено ${count} совпадений`);
+    });
+}
+
+function findAction(filePath) {
+    // Наверное все функции надо переписать на промисах, чтобы 
+    // не было рекурсивных вызовов и стек оставался чистым
+    Promise.resolve().then(async () => {
+        const ans = await inquirer.prompt([
+            {
+                name: "strToFind",
+                type: "input",
+                message: "Введите строку для поиска или рег выражение например: \\w*ti\\w*",
+            },
+        ]);
+        findInStream(filePath, ans.strToFind);
+    });
+}
+
+async function choseAction() {
+    const list = ["1. Ввести полное имя файла", "2. Выбрать из списка"];
+    const answer = await inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "Выберите действие:",
+            choices: list,
+        },
+    ]);
+
+    const n = +answer.action.charAt(0);
+    if (n === 1) {
+        const ans1 = await inquirer.prompt([
+            {
+                name: "path",
+                type: "input",
+                message: "Введите полное имя файла:",
+            },
+        ]);
+        findAction(ans1.path);
+    } else {
+        choseFile();
+    }
 }
 
 async function choseFile() {
@@ -64,26 +93,11 @@ async function choseFile() {
 
     if (!isFile(currentDirectory + '\\' + answer.fileName)) {
         currentDirectory += '\\' + answer.fileName;
-        // console.log('not file');
         choseFile();
     } else {
         const filePath = path.join(currentDirectory, answer.fileName);
-        // fs.readFile(filePath, 'utf8', (err, data) => {
-        //     console.log(data);
-        // });
-
-        const ans2 = await inquirer.prompt([
-            {
-                name: "strToFind",
-                type: "input",
-                message: "Введите строку для поиска:",
-            },
-        ]);
-
-        findInStream(filePath, ans2.strToFind);
+        findAction(filePath);
     }
-
 }
 
-choseFile();//'substringLength'
-
+choseAction();
